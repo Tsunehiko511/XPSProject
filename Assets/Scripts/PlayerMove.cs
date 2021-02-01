6a18a640dca46539492c4c1a17913b241d0748fa
嶋津恒彦 <simanezumi1989@gmail.com>
2021-02-01 21:48:59 +0900

    モデルのインポート

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    float inputX;
+    float inputZ;
+
+    Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        inputX = Input.GetAxis("Horizontal");
+        inputZ = Input.GetAxis("Vertical");
+        Debug.Log(transform.forward);
+        transform.position += (transform.forward * inputZ + transform.right * inputX)*Time.deltaTime*5;
+
+        animator.SetFloat("inputX", inputX);
+        animator.SetFloat("inputZ", inputZ);
+
+        animator.SetFloat("speed", Mathf.Abs(inputX)+ Mathf.Abs(inputZ));
+    }
+}
